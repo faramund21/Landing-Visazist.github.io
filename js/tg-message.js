81693a96edc42c0a90011b6260130219e069af53
faramund21 <faramund21@rambler.ru>
2023-04-05 16:58:58 +0900

    Отправка формы в ТГ консультация

--- js/tg-message.js
+++ js/tg-message.js
@@ -1,37 +1,44 @@
-<script>
-  const TOKEN = "";
-  const CHAT_ID = "";
-  const URI-API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
-  const success = document.getElementById('success');
+"use strict"
 
-  document.getElementById('tg').addEventListener('submit', function(e) {
-    e.preventDefault();
+const TOKEN = "5961037641:AAEg50OiDYTJv3r616SVjzDd0F2yW7gTeqc";
+const CHAT_ID = "-1001965614141";
+const URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
+const success = document.getElementById('success');
 
-    let message = `<b>Заявка с сайта!</b>\n`;
-    message += `<b>Отправитель: </b> ${ this.name.value }\n`;
-    message += `<b>Почта: </b> ${ this.email.value }`;
+document.getElementById('tg').addEventListener('submit', function(e) {
+  e.preventDefault();
 
-    axios.post(URI_API, {
-      chat_id: CHAT_ID,
-      parse_mode: 'html',
-      text: message
-    })
 
-    .then((res) => {
-      this.name.value = "";
-      this.email.value = "";
-      success.innerHTML = "Заявка отправлена!";
-      success.style.display = "block";
-    })
 
-    .catch((err) => {
-      console.warn(err);
-    })
+  let message = `<b>Заявка с сайта!</b>\n`;
+  message += `<b>Отправитель: </b> ${ this.name.value }\n`;
+  message += `<b>Нужна консультация в</b> ${ this.time.value }\n`;
+  message += `<b>Телефон для связи:</b> ${ this.phone.value }`;
 
-    .finally(() => {
-      console.log('Конец');
-    })
+  <!-- console.log(message); -->
+
+  axios.post(URI_API, {
+    chat_id: CHAT_ID,
+    parse_mode: 'html',
+    text: message
   })
-</script>
+
+  .then((res) => {
+    this.name.value = "";
+    this.time.value = "";
+    this.phone.value = "";
+    success.innerHTML = "Заявка отправлена!";
+    success.style.display = "block";
+  })
+
+  .catch((err) => {
+    console.warn(err);
+  })
+
+  .finally(() => {
+    console.log('Конец');
+  })
+
+})
 
 
