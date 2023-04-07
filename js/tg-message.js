690ee86bf1cc820fde6c943d344efca93fb2fccd
faramund21 <faramund21@rambler.ru>
2023-04-07 19:36:30 +0900

    Остальные формы

--- js/tg-message.js
+++ js/tg-message.js
@@ -5,10 +5,8 @@ const CHAT_ID = "-1001965614141";
 const URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
 const success = document.getElementById('success');
 
-document.getElementById('tg').addEventListener('submit', function(e) {
+document.getElementById('tg-consultation').addEventListener('submit', function(e) {
   e.preventDefault();
-
-
 
   let message = `<b>Заявка с сайта!</b>\n`;
   message += `<b>Отправитель: </b> ${ this.name.value }\n`;
@@ -41,4 +39,75 @@ document.getElementById('tg').addEventListener('submit', function(e) {
 
 })
 
+document.getElementById('tg-ask-questions').addEventListener('submit', function(e) {
+  e.preventDefault();
 
+  let message = `<b>Заявка с сайта!</b>\n`;
+  message += `<b>Отправитель: </b> ${ this.name.value }\n`;
+  message += `<b>Телефон для связи:</b> ${ this.phone.value }\n`;
+  message += `<b>Вопрос: </b> ${ this.question.value }`;
+
+  <!-- console.log(message); -->
+
+  axios.post(URI_API, {
+    chat_id: CHAT_ID,
+    parse_mode: 'html',
+    text: message
+  })
+
+  .then((res) => {
+    this.name.value = "";
+    this.time.value = "";
+    this.question.value = "";
+    success.innerHTML = "Заявка отправлена!";
+    success.style.display = "block";
+  })
+
+  .catch((err) => {
+    console.warn(err);
+  })
+
+  .finally(() => {
+    console.log('Конец');
+  })
+
+})
+
+document.getElementById('tg-quiz').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  let message = `<b>Заявка с сайта (quiz)!</b>\n`;
+  message += `<b>Отправитель: </b> ${ this.name.value }\n`;
+  message += `<b>Телефон для связи:</b> ${ this.phone.value }\n`;
+  message += `<b>Услуга: </b> ${ this.services.value }\n`;
+  message += `<b>Мероприятие: </b> ${ this.type_event.value }\n`;
+  message += `<b>Дата: </b> ${ this.date.value }\n`;
+  message += `<b>Время: </b> ${ this.time.value }`;
+
+  <!-- console.log(message); -->
+
+  axios.post(URI_API, {
+    chat_id: CHAT_ID,
+    parse_mode: 'html',
+    text: message
+  })
+
+  .then((res) => {
+    this.name.value = "";
+    this.time.value = "";
+    this.date.value = "";
+    this.type_event.value = "";
+    this.services.value = "";
+    success.innerHTML = "Заявка отправлена!";
+    success.style.display = "block";
+  })
+
+  .catch((err) => {
+    console.warn(err);
+  })
+
+  .finally(() => {
+    console.log('Конец');
+  })
+
+})
