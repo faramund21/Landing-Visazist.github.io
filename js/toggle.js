07642ca4172c5d026421864ce2a415239d6a8635
faramund21 <faramund21@rambler.ru>
2023-04-07 11:08:34 +0900

    Выплывающее меню

---
+++ js/toggle.js
@@ -0,0 +1,18 @@
+var menushow = document.querySelector(".main-nav__toggle");
+var menuclick = document.querySelector(".main-nav__list");
+var popup = document.querySelector(".main-nav");
+var body = document.querySelector(".page__body");
+
+menushow.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popup.classList.toggle("active");
+  menushow.classList.toggle("active");
+  body.classList.toggle("lock");
+});
+
+menuclick.addEventListener("click", function(evt) {
+	// evt.preventDefault();
+	popup.classList.remove("active");
+  menushow.classList.remove("active");
+  body.classList.remove("lock");
+});
