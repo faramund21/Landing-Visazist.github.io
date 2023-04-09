064d40b740191f1a83f28643100a1654f4d8e2d3
faramund21 <faramund21@rambler.ru>
2023-04-09 20:47:13 +0900

    Не получились слайды с примерами работ - начало

--- js/slider.js
+++ js/slider.js
@@ -23,6 +23,7 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
+
   // Автоматическое перелистование
 
   // autoplay: {
@@ -35,13 +36,12 @@ const swiper = new Swiper('.swiper', {
   // },
 });
 
-
-/* const exam = new Swiper('.', {
+const servicesswiper = new Swiper('.services__swiper', {
   // Optional parameters
-  // direction: 'horizontal', // 'vertical',
-  // loop: true,
-  // speed: 1500,
-  // effect: 'cube', // 'cards', 'flip', 'fade', 'coverflow',
+  direction: 'horizontal', // 'vertical',
+  loop: true,
+  speed: 1500,
+  // effect: 'cube', 'cards', 'flip', 'fade', 'coverflow',
   // initialSlide: 1, // НАчинаем со второго слайда
   // freeMode: true, Можно перетаскивать как ленту
   // slidesPerView: 2, // Количество активных слайдов
@@ -55,23 +55,23 @@ const swiper = new Swiper('.swiper', {
 
   // Navigation arrows
   // navigation: {
-    // nextEl: '.swiper-button-next',
-    // prevEl: '.swiper-button-prev',
+    // nextEl: '.swiper-button-next__services',
+    // prevEl: '.swiper-button-prev__services',
   // },
 
   // Автоматическое перелистование
 
   // autoplay: {
-    // delay: 750, // 1с = 1000
+    // delay: 1500, // 1с = 1000
   // },
 
   // And if we need scrollbar
   // scrollbar: {
     // el: '.swiper-scrollbar',
   // },
-}); */
+});
 
-/* const examwrapper = new Swiper('.examwrapper', {
+const examwrapper = new Swiper('.examwrapper', {
   // Optional parameters
   direction: 'horizontal', // 'vertical',
   loop: true,
@@ -104,4 +104,39 @@ const swiper = new Swiper('.swiper', {
   // scrollbar: {
     // el: '.swiper-scrollbar',
   // },
-}); */
+});
+
+const swiperreviews = new Swiper('.swiperreviews', {
+  // Optional parameters
+  direction: 'horizontal', // 'vertical',
+  loop: true,
+  speed: 1500,
+  // effect: 'cube', 'cards', 'flip', 'fade', 'coverflow',
+  // initialSlide: 1, // НАчинаем со второго слайда
+  // freeMode: true, Можно перетаскивать как ленту
+  // slidesPerView: 2, // Количество активных слайдов
+  // centeredSlides: true, // Центрирование слайдов
+
+  // If we need pagination
+  // pagination: {
+    // el: '.swiper-pagination',
+    // clickable: true,
+  // },
+
+  // Navigation arrows
+  // navigation: {
+    // nextEl: '.swiper-button-next__reviews',
+    // prevEl: '.swiper-button-prev__reviews',
+  // },
+
+  // Автоматическое перелистование
+
+  // autoplay: {
+    // delay: 1500, // 1с = 1000
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+    // el: '.swiper-scrollbar',
+  // },
+});
